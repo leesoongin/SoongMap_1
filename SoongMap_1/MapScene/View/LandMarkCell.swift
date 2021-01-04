86ac64c9ba11d13b157e85472012f0ed8059d8ff
mumu <tnddls2ek@naver.com>
2021-01-05 04:24:08 +0900

    랜드마크 collectionView추가 , cell당 그림자 추가

--- SoongMap_1/MapScene/View/LandMarkCell.swift
+++ SoongMap_1/MapScene/View/LandMarkCell.swift
@@ -2,7 +2,7 @@
 //  LandMarkCell.swift
 //  SoongMap_1
 //
-//  Created by 이숭인 on 2021/01/04.
+//  Created by 이숭인 on 2021/01/05.
 //
 
 import UIKit
--- SoongMap_1/MapScene/View/LandMarkListViewController.swift
+++ SoongMap_1/MapScene/View/LandMarkListViewController.swift
@@ -33,14 +33,19 @@ extension LandMarkListViewController : UICollectionViewDataSource {
         cell.backgroundColor =  #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         cell.layer.cornerRadius = 20
         cell.layer.masksToBounds = true
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0.3, height: 1.0)
+        cell.layer.shadowRadius = 2.0
+        cell.layer.shadowOpacity = 0.4
+        cell.layer.masksToBounds = false
         return cell
     }
 }
 
-//extension LandMarkListViewController : UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: 120, height: 50)
-//    }
-//}
+extension LandMarkListViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120, height: 40)
+    }
+}
 
 
